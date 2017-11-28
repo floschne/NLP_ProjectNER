2ba0e41d1e1ffe3360c44974c9754d8d809148d8
Flo <florian.schneider.1992@gmx.de>
2017-11-29 00:55:21 +0100

    added generated features.xml as well as the code to generate the file

--- src/main/java/de/unihamburg/informatik/nlp4web/tutorial/tut5/xml/Features2Xml.java
+++ src/main/java/de/unihamburg/informatik/nlp4web/tutorial/tut5/xml/Features2Xml.java
@@ -17,9 +17,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Features2Xml {
-	public static void generateTokenFeatureExtractors(String filename) throws IOException {
+    public static void generateTokenFeatureExtractors(String filename) throws IOException {
 
-	    List<FeatureExtractor1<Token>> featureExtractors  = new ArrayList<>();
+        List<FeatureExtractor1<Token>> featureExtractors = new ArrayList<>();
 
         TypePathExtractor<Token> stemExtractor = new TypePathExtractor<>(Token.class, "stem/value");
 
@@ -87,7 +87,7 @@ public class Features2Xml {
             FeatureExtractor1<Token> germanOrganizationNames = new FeatureFunctionExtractor<>(
                     new CoveredTextExtractor<Token>(),
                     FeatureFunctionExtractor.BaseFeatures.EXCLUDE,
-                    new NEListExtractor("src/main/resources/ner/germanOrganizationNames.txt", "LOC"));
+                    new NEListExtractor("src/main/resources/ner/germanOrganizationNames.txt", "ORG"));
 
             featureExtractors.add(personNames);
             featureExtractors.add(surNames);
@@ -106,46 +106,47 @@ public class Features2Xml {
         featureExtractors.add(stemExtractor);
         featureExtractors.add(tokenFeatureExtractor);
         featureExtractors.add(contextFeatureExtractor);
-        
 
-		XStream xstream = XStreamFactory.createXStream();
-		String x = xstream.toXML(featureExtractors);
-		x = removeLogger(x);
-		PrintStream ps = new PrintStream(filename);
-		ps.println(x);
-		ps.close();
-	}
 
-	/**
-	 * To make the xml file more readable remove the logger elements
-	 * that are'nt needed
-	 * @param x
-	 * @return
-	 */
-	private static String removeLogger(String x) {
-		StringBuffer buffer = new StringBuffer();
-		String[] lines=x.split("\n");
-		boolean loggerFound=false;
-		for(String l:lines){
-			if(l.trim().startsWith("<logger>")){
-				loggerFound=true;
-			}
-			if(!loggerFound){
-				buffer.append(l);
-				buffer.append("\n");
-			}else{
-				if(l.trim().startsWith("</logger>")){
-					loggerFound=false;
-				}
-			}
-		}
+        XStream xstream = XStreamFactory.createXStream();
+        String x = xstream.toXML(featureExtractors);
+        x = removeLogger(x);
+        PrintStream ps = new PrintStream(filename);
+        ps.println(x);
+        ps.close();
+    }
 
-		return buffer.toString();
-	}
+    /**
+     * To make the xml file more readable remove the logger elements
+     * that are'nt needed
+     *
+     * @param x
+     * @return
+     */
+    private static String removeLogger(String x) {
+        StringBuffer buffer = new StringBuffer();
+        String[] lines = x.split("\n");
+        boolean loggerFound = false;
+        for (String l : lines) {
+            if (l.trim().startsWith("<logger>")) {
+                loggerFound = true;
+            }
+            if (!loggerFound) {
+                buffer.append(l);
+                buffer.append("\n");
+            } else {
+                if (l.trim().startsWith("</logger>")) {
+                    loggerFound = false;
+                }
+            }
+        }
 
-	public static void main(String[] args) throws IOException {
+        return buffer.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
         UIMAFramework.getLogger().log(Level.INFO, "Writing features.xml file!");
-        String featureFileName="src/main/resources/feature/features.xml";
+        String featureFileName = "src/main/resources/feature/features.xml";
         generateTokenFeatureExtractors(featureFileName);
         UIMAFramework.getLogger().log(Level.INFO, "Done: " + featureFileName);
     }
